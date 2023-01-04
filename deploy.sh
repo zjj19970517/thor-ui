1b7ecb90c7cd015a65ed41c88183e549559e55b6
zhangjinjie <1392372716@qq.com>
2023-01-04 15:20:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,15 @@ set -e
 pnpm run docs:build
 
 # 进入待发布的目录
-cd docs/.vitepress/dist
+# cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+git add .
 git commit -m 'deploy'
 
-# 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+# git push -f https://github.com/zjj19970517/thor-ui.git master:gh-pages
+git push -f git@github.com:zjj19970517/thor-ui.git master:gh-pages
 cd -
